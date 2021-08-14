d628a003f2b3b1638b78e125bfdb571c98dee845
张宏权 <hongquan.zhang@outlook.com>
2021-08-14 11:33:08 +0800

    Publishing to gh-pages (publish.sh)

--- deploy.sh
+++ deploy.sh
@@ -7,21 +7,22 @@ then
 fi
 
 echo "Deleting old publication"
-rm -rf public
-mkdir public
-rm -rf .git/worktrees/public/
+rm -rf docs
+mkdir docs
+rm -rf .git/worktrees/docs
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public origin/gh-pages
+git worktree add -B gh-pages docs origin/gh-pages
 
 echo "Removing existing files"
-rm -rf public/*
+rm -rf docs/*
 
 echo "Generating site"
 hugo
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+cd docs && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
 echo "Push to origin"
 git push origin gh-pages
+
